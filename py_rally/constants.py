8ab682cd154c3f3c28723e848122b0df877f9126
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-08-23 23:15:34 +0100

    added project custom types and constants

---
+++ py_rally/constants.py
@@ -0,0 +1,31 @@
+EIP712TYPE = [
+    {'name': 'name', 'type': 'string'},
+    {'name': 'version', 'type': 'string'},
+    {'name': 'chainId', 'type': 'uint256'},
+    {'name': 'verifyingContract', 'type': 'address'},
+]
+FORWARDER_DATA_SIGNED_TYPE = [
+    {'name': 'from', 'type': 'address'},
+    {'name': 'to', 'type': 'address'},
+    {'name': 'value', 'type': 'uint256'},
+    {'name': 'gas', 'type': 'uint256'},
+    {'name': 'nonce', 'type': 'uint256'},
+    {'name': 'data', 'type': 'bytes'},
+    {'name': 'validUntilTime', 'type': 'uint256'},
+]
+RELAY_DATA_SIGNED_TYPE = [
+    {'name': 'maxFeePerGas', 'type': 'uint256'},
+    {'name': 'maxPriorityFeePerGas', 'type': 'uint256'},
+    {'name': 'transactionCalldataGasUsed', 'type': 'uint256'},
+    {'name': 'relayWorker', 'type': 'address'},
+    {'name': 'paymaster', 'type': 'address'},
+    {'name': 'forwarder', 'type': 'address'},
+    {'name': 'paymasterData', 'type': 'bytes'},
+    {'name': 'clientId', 'type': 'uint256'},
+]
+RELAY_REQUEST_SIGNED_TYPE = [
+    *FORWARDER_DATA_SIGNED_TYPE,
+    {'name': 'relayData', 'type': 'RelayData'},
+]
+DOMAIN_SEPARATOR_VERSION = '3'
+OK_RESPONSE = 200
---
+++ py_rally/custom_types.py
@@ -0,0 +1,101 @@
+from dataclasses import dataclass
+from typing import Optional, TypedDict
+
+
+class EIP721DomainType(TypedDict):
+    chainId: int
+    name: str
+    verifyingContract: str
+    version: str
+
+
+class EIP721MessageType(TypedDict):
+    name: str
+    type: str
+
+
+class ConfigContracts(TypedDict):
+    faucet: str
+    rly_erc20: str
+
+
+class GSNTransaction(TypedDict):
+    from_address: str
+    to: str
+    data: str
+    value: int
+    # Hex string
+    gas: Optional[str]
+    # Hex string
+    max_fee_per_gas: str
+    # Hex string
+    max_priority_fee_per_gas: str
+    paymaster_data: str
+    client_id: int
+
+
+class GSNServerConfigPayload(TypedDict):
+    relayWorkerAddress: str
+    relayManagerAddress: str
+    relayHubAddress: str
+    ownerAddress: str
+    minMaxPriorityFeePerGas: str
+    maxMaxFeePerGas: str
+    minMaxFeePerGas: str
+    maxAcceptanceBudget: str
+    chainId: str
+    networkId: str
+    ready: bool
+    version: str
+
+
+ForwarderRequest = TypedDict(
+    'ForwarderRequest',
+    {
+        'from': str,
+        'to': str,
+        'value': str,
+        'gas': str,
+        'nonce': str,
+        'data': str,
+        'validUntilTime': str,
+    },
+)
+
+
+class RelayHttpRequestMetadata(TypedDict):
+    maxAcceptanceBudget: int
+    relayHubAddress: str
+    signature: str
+    approvalData: str
+    relayMaxNonce: int
+    relayLastKnownNonce: int
+    domainSeparatorName: str
+    relayRequestId: str
+
+
+class RelayData(TypedDict):
+    maxFeePerGas: str
+    maxPriorityFeePerGas: str
+    transactionCalldataGasUsed: str
+    relayWorker: str
+    paymaster: str
+    paymasterData: str
+    clientId: str
+    forwarder: str
+
+
+class RelayRequest(TypedDict):
+    request: ForwarderRequest
+    relayData: RelayData
+
+
+class RelayHttpRequest(TypedDict):
+    relayRequest: RelayRequest
+    metadata: RelayHttpRequestMetadata
+
+
+@dataclass(frozen=True)
+class Account:
+    private_key: str
+    address: str
